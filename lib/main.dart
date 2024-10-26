f559a7deb021a1bbe1aeea843d0a3b76f1308e17
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-10-26 11:48:43 -0500

    dice page init

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
+import 'package:diceappg10/pages/dice_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: DicePage(),
       debugShowCheckedModeBanner: false,
     ),
   );
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [Text("Hola mundo ")],
-        ),
-      ),
-    );
-  }
 }
