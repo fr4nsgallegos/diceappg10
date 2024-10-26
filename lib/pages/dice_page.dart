6b94a5d7240352ee320412893e675c4616f595fa
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-10-26 12:08:27 -0500

    tres formas de poner border circulares a mi container

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -14,7 +14,21 @@ class DicePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              color: Colors.red,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                //TRES FORMAS PARA COLOCAR BORDES CIRCULARES A MI CONTAINER
+                // PRIMERA
+                borderRadius: BorderRadius.circular(16),
+
+                //SEGUNDA
+                // borderRadius: BorderRadius.all(
+                //   Radius.circular(16),
+                // ),
+
+                //TERCERA --- coloco el borde totalmente circular
+                // shape: BoxShape.circle,
+              ),
+              // color: Colors.red,
               padding: EdgeInsets.all(16),
               child: Image.asset(
                 "assets/images/dice1.png",
