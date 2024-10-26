cd833b1b588ff84f5ff1ca3ee9a50dc9d5378457
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-10-26 12:00:18 -0500

    first dice

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -4,8 +4,26 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black38,
       appBar: AppBar(
         title: Text("Dice Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              padding: EdgeInsets.all(16),
+              child: Image.asset(
+                "assets/images/dice1.png",
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
